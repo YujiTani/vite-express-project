fb3dfa501c22a2af45515eadb16e81a24dd36e9a
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-12-13 21:26:22 +0900

    ユーザーデータ追加とputルートを追加

--- src/server/controller/users/index.ts
+++ src/server/controller/users/index.ts
@@ -2,10 +2,18 @@ import express from "express";
 
 const app = express();
 
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  gender: string;
+}
+
 // TODO: DBに接続してデータを取得するようにする
-let users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Doe" },
+let users: User[] = [
+  { id: 1, name: "John Doe", email: "john.doe@example.com", age: 20, gender: "male" },
+  { id: 2, name: "Jane Doe", email: "jane.doe@example.com", age: 21, gender: "female" },
 ];
 
 const getUsers = async (req: express.Request, res: express.Response) => {
@@ -18,7 +26,14 @@ const getUserById = async (req: express.Request, res: express.Response) => {
 }
 
 const registerUser = async (req: express.Request, res: express.Response) => {
-  const newUser = { id: users.length + 1, name: req.body.name };
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    gender: req.body.gender
+  };
+
   users.push(newUser);
   res.status(201).json(newUser);
 }
@@ -34,10 +49,36 @@ const updateUserName = async (req: express.Request, res: express.Response) => {
   res.json(user);
 }
 
+const updateUser = async (req: express.Request, res: express.Response) => {
+  const user = users.find((user) => user.id === Number(req.params.id));
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const newUser = {
+    id: user.id,
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    gender: req.body.gender
+  };
+
+  users = users.map((user) =>  {
+    if (user.id === Number(req.params.id)) {
+      return newUser;
+    }
+
+    return user;
+  });
+
+  res.json(newUser);
+}
+
 const deleteUser = async (req: express.Request, res: express.Response) => {
   const user = users.find((user) => user.id === Number(req.params.id));
   users = users.filter((user) => user.id !== Number(req.params.id));
   res.json(user);
 }
 
-export { getUsers, getUserById, registerUser, updateUserName, deleteUser };
+export { getUsers, getUserById, registerUser, updateUser, updateUserName, deleteUser };
--- src/server/router/users/index.ts
+++ src/server/router/users/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { getUsers, getUserById, registerUser, updateUserName, deleteUser } from "@/server/controller/users/index.js";
+import { getUsers, getUserById, registerUser, updateUser, updateUserName, deleteUser } from "@/server/controller/users/index.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', registerUser);
+router.put('/users/:id', updateUser);
 router.patch('/users/:id', updateUserName);
 router.delete('/users/:id', deleteUser);
 
