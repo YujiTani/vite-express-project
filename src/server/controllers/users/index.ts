1dcb80d4b6dbf1bf9a14964272ed3be413cab7a0
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-01-21 23:45:50 +0900

    prismaを使ったデータ操作に変更した

--- src/server/controllers/users/index.ts
+++ src/server/controllers/users/index.ts
@@ -2,84 +2,175 @@ import { Request, Response } from "express";
 
 import { User } from "@/server/types/controller/users.ts";
 import { ApiController } from "@/server/types/common/index.ts";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { handlePrismaError, logQuery } from "../helper.ts";
 
-// TODO: DBに接続してデータを取得するようにする
-let users: User[] = [
-  { id: 1, name: "John Doe", email: "john.doe@example.com", age: 20, gender: "male" },
-  { id: 2, name: "Jane Doe", email: "jane.doe@example.com", age: 21, gender: "female" },
-];
+const prisma = new PrismaClient({
+  // ログを出力する
+  log: ['query', 'info', 'warn', 'error']}
+);
 
+// クエリイベントリスナーを追加
+prisma.$on('query', logQuery)
+
+/**
+ * ユーザー一覧を取得する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns ユーザー一覧
+ */
 export const getUsers: ApiController = async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany(
+      {
+        where: {
+          deletedAt: null
+    },
+    orderBy: {
+      id: 'asc'
+    },
+    take: 50
+    } 
+  );
   return res.json(users);
+  } catch (error) {
+    return handlePrismaError(error, res);
+  }
 }
 
+/**
+ * ユーザーを取得する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns ユーザー
+ */
 export const getUserById: ApiController = async (req: Request, res: Response) => {
-  const user = users.find((user) => user.id === Number(req.params.id));
+  try{
 
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.id),
+        deletedAt: null
+      }
+    });
+    return res.json(user);
+  } catch (error) {
+    return handlePrismaError(error, res);
   }
-
-  return res.json(user);
 }
 
+/**
+ * ユーザーを作成する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns ユーザー
+ */
 export const registerUser: ApiController = async (req: Request, res: Response) => {
-  const newUser: User = {
-    id: users.length + 1,
-    name: req.body.name,
-    email: req.body.email,
-    age: req.body.age,
-    gender: req.body.gender
-  };
-
-  users.push(newUser);
-  return res.status(201).json(newUser);
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        gender: req.body.gender
+      }
+    })
+    return res.status(201).json(newUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
+  }
 }
 
+/**
+ * ユーザーの名前を更新する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns 更新したユーザー
+ */
 export const updateUserName: ApiController = async (req: Request, res: Response) => {
-  const user = users.find((user) => user.id === Number(req.params.id));
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(req.params.id) },
+      data: { name: req.body.name }
+    });
+    return res.json(updatedUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
   }
-
-  user.name = req.body.name;
-  return res.json(user);
 }
 
+/**
+ * ユーザーを更新する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns 更新したユーザー
+ */
 export const updateUser: ApiController = async (req: Request, res: Response) => {
-  const user = users.find((user) => user.id === Number(req.params.id));
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {id: Number(req.params.id)},
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        gender: req.body.gender,
+      }
+    })
+    return res.json(updatedUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
   }
-
-  const newUser: User = {
-    id: user.id,
-    name: req.body.name,
-    email: req.body.email,
-    age: req.body.age,
-    gender: req.body.gender
-  };
-
-  users = users.map((user) =>  {
-    if (user.id === Number(req.params.id)) {
-      return newUser;
-    }
-
-    return user;
-  });
-
-  return res.json(newUser);
 }
 
-export const deleteUser: ApiController = async (req: Request, res: Response) => {
-  const user = users.find((user) => user.id === Number(req.params.id));
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+/**
+ * ユーザーの削除フラグをたてる
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns 削除フラグをたてたユーザー
+ */
+export const trashUser: ApiController = async (req: Request, res: Response) => {
+  try {
+    const trashedUser = await prisma.user.update({
+      where: {id: Number(req.params.id)},
+      data: {deletedAt: new Date()}
+    })
+    return res.json(trashedUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
   }
+}
 
-  users = users.filter((user) => user.id !== Number(req.params.id));
-  return res.json(user);
+/**
+ * ユーザーの削除フラグを解除する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns 削除フラグを解除したユーザー
+ */
+export const restoreUser: ApiController = async (req: Request, res: Response) => {
+  try {
+    const restoredUser = await prisma.user.update({
+      where: {id: Number(req.params.id)},
+      data: {deletedAt: null}
+    })
+    return res.json(restoredUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
+  }
+}
+
+/**
+ * ユーザーを完全削除する
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns 完全削除したユーザー
+ */
+export const destroyUser: ApiController = async (req: Request, res: Response) => {
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: {id: Number(req.params.id)},
+    })
+    return res.json(deletedUser);
+  } catch (error) {
+    return handlePrismaError(error, res);
+  }
 }
