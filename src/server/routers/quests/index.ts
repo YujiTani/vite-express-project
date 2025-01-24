9ac0d5c187b828eb920c166ff3a48b796c8d6e3a
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-01-25 00:00:34 +0900

    バリデーションを作成

--- src/server/routers/quests/index.ts
+++ src/server/routers/quests/index.ts
@@ -1,15 +1,17 @@
 import { requestErrorHandler } from "@/server/controllers/helper.ts";
 import { createQuest, destroyQuestByUuid, getQuestById, getQuests, restoreQuestByUuid, trashQuestByUuid, updateQuestByUuid } from "@/server/controllers/quests/index.ts";
+import { validateRequest } from "@/server/validators/helper.ts";
+import { basicQuestValidation } from "@/server/validators/quests/indext.ts";
 import express from "express";
 
 const router = express.Router();
 
 router.get('/', requestErrorHandler(getQuests));
 router.get('/:id', requestErrorHandler(getQuestById));
-router.post('/', requestErrorHandler(createQuest));
-router.put('/:uuid', requestErrorHandler(updateQuestByUuid));
+router.post('/',[...basicQuestValidation, validateRequest], requestErrorHandler(createQuest));
+router.put('/:uuid', [...basicQuestValidation, validateRequest], requestErrorHandler(updateQuestByUuid));
 router.delete('/:uuid/trash', requestErrorHandler(trashQuestByUuid));
 router.put('/:uuid/restore', requestErrorHandler(restoreQuestByUuid));
-router.delete('/:uuid', requestErrorHandler(destroyQuestByUuid));    
+router.delete('/:uuid', requestErrorHandler(destroyQuestByUuid));
 
 export default router;
